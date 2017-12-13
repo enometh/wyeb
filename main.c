69cf530e5f23902dd3498e87233e1cd574143b45
Madhu <enometh@net.meer>
2017-12-13 15:49:27 +0530

    main.c: access file:// urls from wyeb:history
    	* main.c: (policycb): allow
    	POLICY_DECISION_TYPE_NAVIGATION_ACTION. (newwin): mark
    	webkit_security_manager_register_uri_scheme_as_local a local
    	scheme.
    
    Also set allow-file-access-from-file-urls=true in main.conf
    
    Without this you get `CONSOLE ERROR Not allowed to load local resource:
    file:///etc/passwd'. After this: `Received an unexpected URL from the
    web process: 'file:///etc/passwd'. And `Received an invalid message
    "WebPageProxy.DecidePolicyForNavigationAction" from the web process.'
    
    This still currently fails.  There is a spurious check in
    Source/WebKit/UIProcess/WebProcessProxy.cpp
    
    WebProcessProxy::checkURLReceivedFromWebProcess
    
    with this comment line 515:
    // A Web processthat was never asked to load a file URL should not ever ask us to do anything with a file URL.
        WTFLogAlways("Received an unexpected URL from the web process: '%s'\n", url.string().utf8().data());
    
    This needs to be fixed in WebKit.  This erorr happens the first time a
    file url is clicked.  If it is loaded manually via "open", then file
    urls can be clicked later.
    
    Earlier refs:
    
    https://bugs.webkit.org/show_bug.cgi?format=multiple&id=127576|Full Text Bug Listing
    https://lists.webkit.org/pipermail/webkit-dev/2009-May/007658.html|[webkit-dev] How to let webkit to be able to load local image file in html content?
    https://lists.webkit.org/pipermail/webkit-dev/2009-May/007667.html|[webkit-dev] How to let webkit to be able to load local image file in html content?
    https://lists.webkit.org/pipermail/webkit-dev/2009-May/007686.html|[webkit-dev] How to let webkit to be able to load local image file in html content?
    https://lists.webkit.org/pipermail/webkit-dev/2009-May/007687.html|[webkit-dev] How to let webkit to be able to load local image file in html content?
    https://lists.webkit.org/pipermail/webkit-dev/2009-May/007690.html|[webkit-dev] How to let webkit to be able to load local image file in html content?
    https://lists.webkit.org/pipermail/webkit-dev/2009-May/007691.html|[webkit-dev] How to let webkit to be able to load local image file in html content?
    https://lists.webkit.org/pipermail/webkit-dev/2009-May/007693.html|[webkit-dev] How to let webkit to be able to load local image file in html content?
    https://lists.webkit.org/pipermail/webkit-dev/2009-May/007693.html|[webkit-dev] How to let webkit to be able to load local image file in html content?
    https://lists.webkit.org/pipermail/webkit-dev/2009-May/thread.html#7658|The webkit-dev May 2009 Archive by thread
    https://stackoverflow.com/questions/1834122/using-local-path-with-webview-webkit|gtk - Using local path with WebView (Webkit) - Stack Overflow
    https://stackoverflow.com/questions/40123321/wkwebview-run-local-html-in-document-received-an-unexpected-url-from-the-web-p|iframe - wkwebview run local html (in document) Received an unexpected URL from the web process - Stack Overflow

--- main.c
+++ main.c
@@ -3961,6 +3961,9 @@ static gboolean policycb(
 		//it changes uri of wins and can't recover.
 		webkit_policy_decision_download(dec);
 		return true;
+	} else 	if (type == WEBKIT_POLICY_DECISION_TYPE_NAVIGATION_ACTION) {
+		webkit_policy_decision_use(dec);
+		return true;
 	}
 
 	if (type != WEBKIT_POLICY_DECISION_TYPE_RESPONSE)
