55eb52f6f1760f7df679e647ab570189870b7748
Madhu <enometh@net.meer>
2023-02-12 06:45:12 +0530

    M-x readermode
    https://github.com/mozilla/readability by way of the implementation in
    Epiphany.

--- main.c
+++ main.c
@@ -2929,6 +2929,8 @@ xhtml_to_html5(Win *win)
 		webkit_web_resource_get_data(res, NULL, xhtml_to_html5_resourcecb, win);
 }
 
+void readability_mode(Win *win);
+#include "readability/readability.c"
 
 //@actions
 typedef struct {
@@ -3626,6 +3628,7 @@ static bool _run(Win *win, const char* action, const char *arg, char *cdir, char
 
 
 	Z("reloadashtml5", xhtml_to_html5(win));
+	Z("readermode", readability_mode(win));
 
 	Z("quit"        , gtk_widget_destroy(win->winw); return true)
 
